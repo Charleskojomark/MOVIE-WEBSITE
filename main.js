5d7a0e55cbc63602979ebe4b44137a19bcac47b9
Charleskojomark <markcharleskojo@gmail.com>
2024-03-23 13:31:36 +0100

    Movies static page

---
+++ main.js
@@ -0,0 +1,58 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', ()=>{
+    header.classList.toggle('shadow', window.scrollY > 0);
+})
+
+menu.onclick = ()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = ()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active')
+}
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".coming-container", {
+    spaceBetween: 20,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        360: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        968: {
+            slidesPerView: 5,
+        },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
